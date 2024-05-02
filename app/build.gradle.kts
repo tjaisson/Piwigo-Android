23a6abe3f47cdd6aa94b346031e5e05fc90f8894
tjaisson <thomas.jaisson@ac-paris.fr>
2024-05-02 18:37:55 +0200

    library catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,22 +34,18 @@ android {
         dataBinding = true
         buildConfig = true
         // for view binding:
-        // viewBinding true
+        // viewBinding = true
     }
     namespace = "org.piwigo"
 }
 
-val picassoVersion = "2.8"
-val assertjVersion = "1.2.0"
-val espressoVersion = "3.3.0"
-
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.annotation:annotation:1.1.0")
-    implementation("com.google.android.material:material:1.2.1")
-    implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.annotation)
+    implementation(libs.android.material)
+    implementation(libs.androidx.cardview)
+    implementation(libs.androidx.swiperefreshlayout)
     implementation(libs.lifecycle.viewmodel)
     annotationProcessor(libs.lifecycle.compiler)
 
@@ -65,9 +61,9 @@ dependencies {
     implementation(libs.retrofit.converter.gson)
     implementation(libs.retrofit.adapter.rxjava2)
 
-    implementation("com.squareup.picasso:picasso:${picassoVersion}")
-    implementation("io.reactivex.rxjava2:rxandroid:2.1.1")
-    implementation("io.reactivex.rxjava2:rxjava:2.2.20")
+    implementation(libs.picasso)
+    implementation(libs.rx.android)
+    implementation(libs.rx.java)
     implementation(libs.guava)
     annotationProcessor(libs.guava)
 
@@ -77,39 +73,39 @@ dependencies {
     implementation(libs.room.guava)
 
 
-    implementation("org.apache.commons:commons-lang3:3.11")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("com.vipulasri:ticketview:1.0.7")
-    implementation("androidx.preference:preference:1.1.1")
+    implementation(libs.apache.lang3)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.vipulasri.ticketview)
+    implementation(libs.androidx.preference)
     
-    implementation("com.github.jorgecastilloprz:fabprogresscircle:1.01@aar")
-    implementation("com.leinardi.android:speed-dial:3.3.0")
-    implementation("com.github.tingyik90:snackprogressbar:6.1.1")
-    implementation("org.greenrobot:eventbus:3.2.0")
+    implementation(libs.fabprogresscircle)
+    implementation(libs.speedDial)
+    implementation(libs.snackprogressbar)
+    implementation(libs.eventbus)
     implementation(libs.kotlin.stdlib.jdk8)
     /* Don't forget to add to string libraries if you add a library here. */
 
 //    debugImplementation 'com.squareup.leakcanary:leakcanary-android:2.0-beta-3'
 
-    testImplementation("junit:junit:4.13")
-    testImplementation("org.robolectric:robolectric:4.4")
-    testImplementation("com.squareup.assertj:assertj-android:${assertjVersion}")
+    testImplementation(libs.junit)
+    testImplementation(libs.robolectric)
+    testImplementation(libs.assertj)
     testAnnotationProcessor(libs.dagger.compiler)
-    testImplementation("androidx.arch.core:core-testing:2.1.0")
-    testImplementation("org.mockito:mockito-core:3.5.13")
-    testImplementation("com.google.guava:guava:29.0-android")
-    testImplementation("androidx.appcompat:appcompat:1.2.0")
-    testAnnotationProcessor("com.google.guava:guava:29.0-android")
-    testImplementation("com.google.code.findbugs:jsr305:3.0.2")
+    testImplementation(libs.androidx.core.testing)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.guava)
+    testImplementation(libs.androidx.appcompat)
+    testAnnotationProcessor(libs.guava)
+    testImplementation(libs.jsr305)
 
-    androidTestImplementation("androidx.test:core:1.4.0")
-    androidTestImplementation("androidx.test:core-ktx:1.4.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2-alpha05")
-    androidTestImplementation("androidx.test:runner:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:${espressoVersion}")
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:${espressoVersion}")
-    androidTestImplementation("androidx.test:rules:1.3.0")
-    androidTestImplementation("com.android.support.test.uiautomator:uiautomator-v18:2.1.3")
+    androidTestImplementation(libs.androidx.core)
+    androidTestImplementation(libs.androidx.core.ktx)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.espresso.contrib)
+    androidTestImplementation(libs.androidx.test.rules)
+    androidTestImplementation(libs.uiautomator)
     // Note that espresso-idling-resource is used in the code under test.
-    implementation("androidx.test.espresso:espresso-idling-resource:${espressoVersion}")
+    implementation(libs.androidx.espresso.idling.resource)
 }
